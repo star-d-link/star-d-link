7226295c15a49e6ac5af996dc0f88b8781dc5af5
sangwopark19 <sangwopark19@gmail.com>
2024-11-05 18:25:31 +0900

    feat: 자동 생성, 수정일자 생성을 위해 공통 어노테이션 추가

--- src/main/java/com/udemy/star_d_link/StarDLinkApplication.java
+++ src/main/java/com/udemy/star_d_link/StarDLinkApplication.java
@@ -2,8 +2,10 @@ package com.udemy.star_d_link;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class StarDLinkApplication {
 
 	public static void main(String[] args) {
