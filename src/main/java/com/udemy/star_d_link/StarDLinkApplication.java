722dcb01f0c88cf429cd9149929192665a0b8110
sangwopark19 <sangwopark19@gmail.com>
2024-11-05 18:15:24 +0900

    git init

---
+++ src/main/java/com/udemy/star_d_link/StarDLinkApplication.java
@@ -0,0 +1,13 @@
+package com.udemy.star_d_link;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarDLinkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarDLinkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/udemy/star_d_link/StarDLinkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.udemy.star_d_link;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarDLinkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
