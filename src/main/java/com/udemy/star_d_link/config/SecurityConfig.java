c086095b66d01d22184ad61aabb658b872c32bef
sangwopark19 <sangwopark19@gmail.com>
2024-11-05 18:25:04 +0900

    feat: 공통 SecurityConfig 설정

---
+++ src/main/java/com/udemy/star_d_link/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.udemy.star_d_link.config;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter.XFrameOptionsMode;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+@Log4j2
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeHttpRequests(
+                (auth) -> auth.requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
+            .csrf((csrf) -> csrf.ignoringRequestMatchers(
+                new AntPathRequestMatcher("/h2-console/**")))
+            .headers((headers) -> headers.addHeaderWriter(new XFrameOptionsHeaderWriter(
+                XFrameOptionsMode.SAMEORIGIN)));
+
+        return httpSecurity.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
