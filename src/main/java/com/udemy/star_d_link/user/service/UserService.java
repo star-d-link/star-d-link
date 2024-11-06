2fa429e072820d78afe65564214c56325671f22b
sangwopark19 <sangwopark19@gmail.com>
2024-11-06 18:52:54 +0900

    feat: user의 인증정보를 확인하는 기능구현

---
+++ src/main/java/com/udemy/star_d_link/user/service/UserService.java
@@ -0,0 +1,9 @@
+package com.udemy.star_d_link.user.service;
+
+import com.udemy.star_d_link.user.dto.UserDTO;
+
+public interface UserService {
+
+    UserDTO read(String username, String password);
+
+}
---
+++ src/main/java/com/udemy/star_d_link/user/service/UserServiceImpl.java
@@ -0,0 +1,33 @@
+package com.udemy.star_d_link.user.service;
+
+import com.udemy.star_d_link.user.dto.UserDTO;
+import com.udemy.star_d_link.user.entity.UserEntity;
+import com.udemy.star_d_link.user.exception.UserExceptions;
+import com.udemy.star_d_link.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@RequiredArgsConstructor
+@Log4j2
+@Transactional
+public class UserServiceImpl implements UserService {
+
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    @Override
+    public UserDTO read(String username, String password) {
+        UserEntity userEntity = userRepository.findByUsername(username)
+            .orElseThrow(UserExceptions.NOT_FOUND::get);
+
+        if (!passwordEncoder.matches(password, userEntity.getPassword())) {
+            throw UserExceptions.BAD_CREDENTIALS.get();
+        }
+
+        return new UserDTO(userEntity);
+    }
+}
