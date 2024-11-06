b0bddc1be3103f43ae0af56cb38d12a2d1cb527a
sangwopark19 <sangwopark19@gmail.com>
2024-11-06 18:22:54 +0900

    feat: UserDTO 생성

---
+++ src/main/java/com/udemy/star_d_link/user/dto/UserDTO.java
@@ -0,0 +1,53 @@
+package com.udemy.star_d_link.user.dto;
+
+import com.udemy.star_d_link.user.entity.UserEntity;
+import java.util.Map;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+
+    private String username;
+    private String password;
+    private String nickname;
+    private String email;
+    private String profileUrl;
+    private String phoneNumber;
+    private String birthDate;
+    private String region;
+    private String role;
+    private String createdAt;
+    private String updatedAt;
+
+    public UserDTO(UserEntity userEntity) {
+        this.username = userEntity.getUsername();
+        this.password = userEntity.getPassword();
+        this.nickname = userEntity.getNickname();
+        this.email = userEntity.getEmail();
+        this.profileUrl = userEntity.getProfileUrl();
+        this.phoneNumber = userEntity.getPhoneNumber();
+        this.birthDate = userEntity.getBirthDate();
+        this.region = userEntity.getRegion();
+        this.role = userEntity.getRole();
+    }
+
+    public Map<String, Object> getDataMap() {
+        return Map.of(
+            "username", username,
+            "password", password,
+            "nickname", nickname,
+            "email", email,
+            "profileUrl", profileUrl,
+            "phoneNumber", phoneNumber,
+            "birthDate", birthDate,
+            "region", region,
+            "role", role
+        );
+    }
+}
