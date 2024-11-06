694d776e286e6026c0c127713b68b1151fcdd5bd
sangwopark19 <sangwopark19@gmail.com>
2024-11-06 15:53:23 +0900

    fix: UserTaskException RuntimeException 상속

--- src/main/java/com/udemy/star_d_link/user/exception/UserTaskException.java
+++ src/main/java/com/udemy/star_d_link/user/exception/UserTaskException.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 
 @Getter
 public class UserTaskException extends RuntimeException {
-public class UserTaskException {
 
     private final String msg;
     private final int code;
