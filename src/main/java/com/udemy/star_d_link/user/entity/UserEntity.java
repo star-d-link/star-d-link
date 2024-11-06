e2b4ac85d9804a9d51fdc66668e6cdf40ebea564
sangwopark19 <sangwopark19@gmail.com>
2024-11-06 14:52:22 +0900

    feat: UserEntity 추가

---
+++ src/main/java/com/udemy/star_d_link/user/entity/UserEntity.java
@@ -0,0 +1,67 @@
+package com.udemy.star_d_link.user.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.time.LocalDateTime;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "users")
+@Getter
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@EntityListeners(AuditingEntityListener.class)
+public class UserEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private String id;
+
+    @Column(nullable = false, unique = true, length = 20)
+    private String username;
+
+    @Column(nullable = false, length = 250)
+    private String password;
+
+    @Column(nullable = false, length = 40)
+    private String nickname;
+
+    @Column(nullable = false, length = 40)
+    private String email;
+
+    @Column(length = 250)
+    private String profile_url;
+
+    @Column(nullable = false, length = 20)
+    private String phone_number;
+
+    @Column(nullable = false, length = 20)
+    private String birth_date;
+
+    @Column(nullable = false, length = 100)
+    private String region;
+
+    @Column(nullable = false, length = 20)
+    private String role;
+
+    @CreatedDate
+    private LocalDateTime created_at;
+
+    @LastModifiedDate
+    private LocalDateTime updated_at;
+
+}
