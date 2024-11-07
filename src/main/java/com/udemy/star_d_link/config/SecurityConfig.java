0f6f1f67983b4544ba2fc5527d3ddf28d695c394
sangwopark19 <sangwopark19@gmail.com>
2024-11-07 12:57:03 +0900

    feat: update SecurityConfig for Method Security

--- src/main/java/com/udemy/star_d_link/config/SecurityConfig.java
+++ src/main/java/com/udemy/star_d_link/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.udemy.star_d_link.config;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -14,6 +15,7 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 @Log4j2
 public class SecurityConfig {
 
