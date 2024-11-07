ab852fb7c38649a582d45d2ea29e5da3680f541d
sangwopark19 <sangwopark19@gmail.com>
2024-11-07 12:58:59 +0900

    feat: add handleAccessDeniedException

--- src/main/java/com/udemy/star_d_link/user/controller/advice/TokenControllerAdvice.java
+++ src/main/java/com/udemy/star_d_link/user/controller/advice/TokenControllerAdvice.java
@@ -1,9 +1,12 @@
 package com.udemy.star_d_link.user.controller.advice;
 
 import com.udemy.star_d_link.user.exception.UserTaskException;
+import java.util.HashMap;
 import java.util.Map;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -22,5 +25,16 @@ public class TokenControllerAdvice {
         Map<String, String> map = Map.of("error", msg);
 
         return ResponseEntity.status(status).body(map);
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public ResponseEntity<?> handleAccessDeniedException(AccessDeniedException ex) {
+
+        log.info("handleAccessDeniedException..............................");
+
+        Map<String, Object> errors = new HashMap<>();
+        errors.put("message", ex.getMessage());
+
+        return new ResponseEntity<>(errors, HttpStatus.FORBIDDEN);
     }
 }
