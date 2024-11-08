4ccfaa6f840e88dd2adf0de7bdc0a7a9ae348280
sangwopark19 <sangwopark19@gmail.com>
2024-11-08 16:12:59 +0900

    feat: add 액세스 토큰의 정보로 AuthenticationToekn객체 생성하는 기능

--- src/main/java/com/udemy/star_d_link/security/filter/JWTCheckFilter.java
+++ src/main/java/com/udemy/star_d_link/security/filter/JWTCheckFilter.java
@@ -1,14 +1,21 @@
 package com.udemy.star_d_link.security.filter;
 
+import com.udemy.star_d_link.security.auth.CustomUserPrincipal;
 import com.udemy.star_d_link.security.util.JWTUtil;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.Map;
+import java.util.stream.Collectors;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -67,8 +74,31 @@ public class JWTCheckFilter extends OncePerRequestFilter {
 
         try {
             Map<String, Object> tokenMap = jwtUtil.validateToken(accessToken);
-            // 토큰 검증 결과에 문제가 없으면 다음 필터로 넘어간다.
+            // 토큰 검증 결과에 문제가 없으면 tokenMap에서 사용자 정보를 가져와
+            // Authentication 객체를 생성한다.
             log.info("tokenMap: " + tokenMap);
+
+            String username = tokenMap.get("username").toString();
+            //권한이 여러 개인 경우에는 ,로 구분해서 처리한다
+            String[] roles = tokenMap.get("roles").toString().split(",");
+
+            // 토큰 검증 결과를 이용해 Authentication 객체를 생성한다.
+            // Access Token을 이용해서 이미 검사가 완료되었으므로 Credentials는 null로 지정
+            UsernamePasswordAuthenticationToken authenticationToken =
+                new UsernamePasswordAuthenticationToken(
+                    new CustomUserPrincipal(username),
+                    null,
+                    Arrays.stream(roles)
+                        .map(SimpleGrantedAuthority::new)
+                        .collect(Collectors.toList())
+                );
+
+            //SecurityContextHolder에 Authentication 객체를 저장
+            //나중에 SecurityContextHolder를 이용해서 Authentication 객체를 꺼내서 사용할 수 있다.
+            SecurityContext context = SecurityContextHolder.getContext();
+            context.setAuthentication(authenticationToken);
+
+            // 완료 후 다음 필터로 넘어간다. (다음필터가 없으면 컨트롤러 요청으로 넘어간다.
             filterChain.doFilter(request, response);
         } catch (Exception e) {
             // 문제가 발생하면
