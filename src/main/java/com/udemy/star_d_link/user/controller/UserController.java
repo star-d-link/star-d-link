8505a2299dbd41c86011c5a8744bd7d958d0983f
sangwopark19 <sangwopark19@gmail.com>
2024-11-08 17:13:03 +0900

    feat: UserController ROLE_ 접두사 추가

--- src/main/java/com/udemy/star_d_link/user/controller/UserController.java
+++ src/main/java/com/udemy/star_d_link/user/controller/UserController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 public class UserController {
 
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     @GetMapping
     public ResponseEntity<?> list() {
 
