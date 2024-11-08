97f9b46b719f89df7b354b66e792b83977b6459b
shb <shb2891@gmail.com>
2024-11-08 23:27:04 +0900

    fix: 제네릭 추가

--- src/main/java/com/udemy/star_d_link/Repository/CourseReviewRepository.java
+++ src/main/java/com/udemy/star_d_link/Repository/CourseReviewRepository.java
@@ -2,11 +2,16 @@ package com.udemy.star_d_link.Repository;
 
 import com.udemy.star_d_link.Entity.CourseReview;
 import java.util.List;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CourseReviewRepository extends JpaRepository {
-    List<CourseReview> findAll();
-    List<CourseReview> findBySiteUser(SiteUser siteuser);
+public interface CourseReviewRepository extends JpaRepository<CourseReview, Integer> {
+    Page<CourseReview> findAll(Pageable pageable);
+
+    List<CourseReview> findByUserId(Integer userId);//나중에 삭제
+    /*List<CourseReview> findBySiteUser(SiteUser siteuser);*/
+
     List<CourseReview> findByTitle(String title);
     List<CourseReview> findByContent(String Content);
     List<CourseReview> findByHashtag(String hashTag);
