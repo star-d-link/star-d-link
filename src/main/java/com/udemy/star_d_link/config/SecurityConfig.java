1e345249ceec107bfcc2854268e10eb510ed519b
sangwopark19 <sangwopark19@gmail.com>
2024-11-08 14:16:47 +0900

    feat: update SecurityConfig for jwtCheckFilter

--- src/main/java/com/udemy/star_d_link/config/SecurityConfig.java
+++ src/main/java/com/udemy/star_d_link/config/SecurityConfig.java
@@ -1,15 +1,20 @@
 package com.udemy.star_d_link.config;
 
+import com.udemy.star_d_link.security.filter.JWTCheckFilter;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter.XFrameOptionsMode;
 
@@ -17,10 +22,13 @@ import org.springframework.security.web.header.writers.frameoptions.XFrameOption
 @EnableWebSecurity
 @EnableMethodSecurity(prePostEnabled = true)
 @Log4j2
+@RequiredArgsConstructor
 public class SecurityConfig {
 
+    private final JWTCheckFilter jwtCheckFilter;
+
     @Bean
-    SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity
             // 폼 로그인 비활성화
             .formLogin(AbstractHttpConfigurer::disable)
@@ -29,7 +37,10 @@ public class SecurityConfig {
             // csrf 토큰 세션 비활성화
             .csrf(AbstractHttpConfigurer::disable)
             // Http session 생성 비활성화
-            .sessionManagement(AbstractHttpConfigurer::disable)
+            .sessionManagement(sessionManagementConfigurer -> {
+                sessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.NEVER);
+            })
+            .addFilterBefore(jwtCheckFilter, UsernamePasswordAuthenticationFilter.class)
             // 모든 url 접근허용
 //            .authorizeHttpRequests(
 //                (auth) -> auth.requestMatchers(new AntPathRequestMatcher("/**")).permitAll())
