ff179ebf8ef56e4ee159ee10732e92dfe92ec1e4
shb <shb2891@gmail.com>
2024-11-19 14:43:08 +0900

    fix: 오타 수정

--- src/main/java/com/udemy/star_d_link/coursereview/Repository/CourseReviewFileRepository.java
+++ src/main/java/com/udemy/star_d_link/coursereview/Repository/CourseReviewFileRepository.java
@@ -6,5 +6,5 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface CourseReviewFileRepository extends JpaRepository<CourseReviewFile, Integer> {
-    List<CourseReviewFile> findByCourseReivew(CourseReview courseReview);
+    List<CourseReviewFile> findByCourseReview(CourseReview courseReview);
 }
