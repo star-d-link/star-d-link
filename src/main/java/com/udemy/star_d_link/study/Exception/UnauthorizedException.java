1c2bd2f370378388aa2616e4bf4649a82ef54143
Sangwoo Park <39483805+sangwopark19@users.noreply.github.com>
2024-11-19 15:07:11 +0900

    Revert "Study"

--- src/main/java/com/udemy/star_d_link/study/Exception/UnauthorizedException.java
+++
@@ -1,7 +0,0 @@
-package com.udemy.star_d_link.study.Exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
