9e4f9616c5d02ec0c219f07742a132aed5bfda76
GunBan00 <58806686+GunBan00@users.noreply.github.com>
2024-11-21 13:01:47 +0900

    fix: User DTO, Entity에 맞춰서 Service 계층 수정

---
+++ src/main/java/com/udemy/star_d_link/study/Service/StudyMembersService.java
@@ -0,0 +1,125 @@
+package com.udemy.star_d_link.study.Service;
+
+import com.udemy.star_d_link.study.Entity.Role;
+import com.udemy.star_d_link.study.Entity.Study;
+import com.udemy.star_d_link.study.Entity.StudyMembers;
+import com.udemy.star_d_link.study.Exception.UnauthorizedException;
+import com.udemy.star_d_link.study.Repository.StudyMemberRepository;
+import com.udemy.star_d_link.study.Repository.StudyRepository;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class StudyMembersService {
+    private final StudyMemberRepository studyMemberRepository;
+    private final StudyRepository studyRepository;
+    @Autowired
+    public StudyMembersService(StudyMemberRepository studyMemberRepository, StudyRepository studyRepository) {
+        this.studyMemberRepository = studyMemberRepository;
+        this.studyRepository = studyRepository;
+    }
+
+    @Transactional
+    public StudyMembers applyStudy(Long studyId, String username) {
+        Study study = studyRepository.findById(studyId)
+            .orElseThrow(() -> new NoSuchElementException("해당 스터디를 찾을 수 없습니다: "));
+
+        boolean alreadyApplied = studyMemberRepository.existsByUsernameAndStudy(username, study);
+        if (alreadyApplied) {
+            throw new IllegalArgumentException("이미 스터디에 신청하셨습니다.");
+        }
+
+        Role role = study.getUsername().equals(username) ? Role.LEADER : Role.MEMBER;
+        StudyMembers studyMember = new StudyMembers(username, study, role, "대기중");
+        return studyMemberRepository.save(studyMember);
+    }
+
+    @Transactional(readOnly = true)
+    public Page<StudyMembers> getMemberList(int page, int size) {
+        Pageable pageable = PageRequest.of(page, size, Sort.by("createDate").descending());
+        return studyMemberRepository.findAll(pageable);
+    }
+
+    @Transactional
+    public StudyMembers acceptMember(Long studyId, String proposer) {
+        StudyMembers member = studyMemberRepository.findByUsername(proposer)
+            .orElseThrow(() -> new NoSuchElementException("해당 멤버를 찾을 수 없습니다: "));
+
+        if (!member.getStudy().getStudyId().equals(studyId)) {
+            throw new IllegalArgumentException("해당 스터디에 속하지 않는 멤버입니다.");
+        }
+
+        member = member.toBuilder()
+            .status("참여중")
+            .build();
+
+        return studyMemberRepository.save(member);
+    }
+
+    public void rejectMember(Long studyId, String username) {
+        StudyMembers member = studyMemberRepository.findByUsername(username)
+            .orElseThrow(() -> new NoSuchElementException("해당 멤버를 찾을 수 없습니다: "));
+
+        if(!member.getStudy().getStudyId().equals(studyId)) {
+            throw new IllegalArgumentException("해당 스터디에 속하지 않는 멤버입니다.");
+        }
+
+        studyMemberRepository.delete(member);
+    }
+
+    public StudyMembers changeMemberRole(Long studyId, String username, Role newRole, String memberName) {
+        Study study = studyRepository.findById(studyId)
+            .orElseThrow(() -> new NoSuchElementException("해당 스터디를 찾을 수 없습니다: "));
+
+        StudyMembers currentMember = studyMemberRepository.findByUsernameAndStudy(username, study)
+            .orElseThrow(() -> new UnauthorizedException("권한이 없습니다."));
+
+        if (currentMember.getRole() != Role.LEADER && currentMember.getRole() != Role.SUB_LEADER) {
+            throw new UnauthorizedException("멤버 역할을 변경할 권한이 없습니다.");
+        }
+
+        StudyMembers targetMember = studyMemberRepository.findByUsernameAndStudy(memberName, study)
+            .orElseThrow(() -> new NoSuchElementException("해당 멤버를 찾을 수 없습니다."));
+
+        targetMember = targetMember.toBuilder()
+            .role(newRole)
+            .build();
+        return studyMemberRepository.save(targetMember);
+    }
+
+    public boolean hasPermission(Long studyId, String username) {
+
+        Study study = studyRepository.findByStudyId(studyId)
+            .orElseThrow(() -> new NoSuchElementException("해당 스터디를 찾을 수 없습니다"));
+
+        return study.getUsername().equals(username);
+    }
+
+    public boolean hasManagementPermission(Long studyId, String username) {
+        Study study = studyRepository.findById(studyId)
+            .orElseThrow(() -> new NoSuchElementException("해당 스터디를 찾을 수 없습니다"));
+
+        StudyMembers studyMember = studyMemberRepository.findByUsernameAndStudy(username, study)
+            .orElseThrow(() -> new NoSuchElementException("해당 스터디에 사용자가 가입되어 있지 않습니다."));
+
+        // 사용자가 리더 또는 부리더인지 확인
+        return studyMember.getRole() == Role.LEADER || studyMember.getRole() == Role.SUB_LEADER;
+    }
+
+    public boolean isMemberOfStudy(Long studyId, String username) {
+
+        Study study = studyRepository.findById(studyId)
+            .orElseThrow(() -> new NoSuchElementException("해당 스터디를 찾을 수 없습니다: "));
+
+        Optional<StudyMembers> studyMember = studyMemberRepository.findByStudyAndUsernameAndStatusNot(study, username, "대기중");
+
+        return studyMember.isPresent();
+    }
+}
---
+++ src/main/java/com/udemy/star_d_link/study/Service/StudyService.java
@@ -0,0 +1,130 @@
+package com.udemy.star_d_link.study.Service;
+
+import com.querydsl.core.BooleanBuilder;
+import com.udemy.star_d_link.study.Dto.Request.StudyCreateRequestDto;
+import com.udemy.star_d_link.study.Dto.Request.StudyUpdateRequestDto;
+import com.udemy.star_d_link.study.Entity.QStudy;
+import com.udemy.star_d_link.study.Entity.Study;
+import com.udemy.star_d_link.study.Exception.UnauthorizedException;
+import com.udemy.star_d_link.study.Repository.StudyRepository;
+import com.udemy.star_d_link.user.repository.UserRepository;
+import java.util.NoSuchElementException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class StudyService {
+
+    private final StudyRepository studyRepository;
+    private final UserRepository userRepository;
+    public StudyService(StudyRepository studyRepository, UserRepository userRepository) {
+        this.studyRepository = studyRepository;
+        this.userRepository = userRepository;
+
+    }
+    @Transactional
+    public Study createStudy(StudyCreateRequestDto requestDto, String username) {
+
+        Study study = requestDto.toEntity(username);
+        return studyRepository.save(study);
+    }
+
+    public Study getStudyForEdit(Long studyId, String username) {
+
+        Study study = studyRepository.findById(studyId)
+            .orElseThrow(() -> new RuntimeException("해당 글을 찾을 수 없습니다."));
+
+        if (!study.getUsername().equals(username)) {
+            throw new UnauthorizedException(" 권한이 없습니다.");
+        }
+
+        return study;
+    }
+
+    @Transactional
+    public Study editStudyByUserId(Long studyId, String username, StudyUpdateRequestDto requestDto) {
+        Study study = studyRepository.findById(studyId)
+            .orElseThrow(() -> new RuntimeException("해당 글을 찾을 수 없습니다."));
+
+        if (!study.getUsername().equals(username)) {
+            throw new UnauthorizedException("수정 권한이 없습니다.");
+        }
+
+        study = study.toBuilder()
+            .title(requestDto.getTitle())
+            .content(requestDto.getContent())
+            .hashtag(requestDto.getHashtag())
+            .isRecruit(requestDto.getIsRecruit())
+            .region(requestDto.getRegion())
+            .isOnline(requestDto.getIsOnline())
+            .headCount(requestDto.getHeadCount())
+            .build();
+
+        return studyRepository.save(study);
+    }
+
+    @Transactional
+    public void deleteStudyByUserId(Long studyId, String username) {
+
+        Study study = studyRepository.findById(studyId)
+            .orElseThrow(() -> new RuntimeException("해당 글을 찾을 수 없습니다."));
+
+        if (!study.getUsername().equals(username)) {
+            throw new UnauthorizedException("해당 글을 삭제할 권한이 없습니다.");
+        }
+
+        studyRepository.delete(study);
+    }
+
+    public Study findByStudyId(Long studyId) {
+        return studyRepository.findByStudyId(studyId)
+            .orElseThrow(() -> new NoSuchElementException("스터디 모집글 내용을 찾을 수 없습니다: " + studyId));
+    }
+
+    public Page<Study> getStudyList(Boolean isOnline, String region, Boolean isRecruit, Pageable pageable) {
+        QStudy study = QStudy.study;
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (isOnline != null) {
+            builder.and(study.isOnline.eq(isOnline));
+        }
+        if (region != null) {
+            builder.and(study.region.eq(region));
+        }
+        if (isRecruit != null) {
+            builder.and(study.isRecruit.eq(isRecruit));
+        }
+
+        return studyRepository.findAll(builder, pageable);
+    }
+
+
+    public Page<Study> searchStudy(String keyword, Pageable pageable) {
+        QStudy study = QStudy.study;
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (keyword != null && !keyword.trim().isEmpty()) {
+            builder.or(study.title.containsIgnoreCase(keyword));
+            builder.or(study.content.containsIgnoreCase(keyword));
+        }
+
+        return studyRepository.findAll(builder, pageable);
+    }
+
+    public Page<Study> detailedSearchStudy(String hashtag, Pageable pageable) {
+        QStudy study = QStudy.study;
+
+        BooleanBuilder builder = new BooleanBuilder();
+
+        if (hashtag != null && !hashtag.trim().isEmpty()) {
+            builder.and(study.hashtag.containsIgnoreCase(hashtag));
+        }
+
+        return studyRepository.findAll(builder, pageable);
+    }
+
+    // 임시로 User를 조회하는 메서드들 추후 변경
+}
