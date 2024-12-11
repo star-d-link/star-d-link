c5b2772492ac895c670571472e0b88161c3348c4
sangwopark19 <sangwopark19@gmail.com>
2024-12-11 17:17:39 +0900

    feat: OAuth2 login google, github, kakao, naver 기능 구현

--- src/main/java/com/udemy/star_d_link/user/entity/UserEntity.java
+++ src/main/java/com/udemy/star_d_link/user/entity/UserEntity.java
@@ -31,7 +31,7 @@ public class UserEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 20)
+    @Column(nullable = false, unique = true, length = 50)
     private String username;
 
     @Column(nullable = false, length = 250)
