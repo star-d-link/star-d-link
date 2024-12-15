8e8b2b46d71f11d7e83a0420189f5f2119466dc0
GunBan00 <58806686+GunBan00@users.noreply.github.com>
2024-12-15 11:42:47 +0900

    feat: 미래 시간만 가능

--- src/main/java/com/udemy/star_d_link/study/Dto/Request/StudyScheduleCreateRequestDto.java
+++ src/main/java/com/udemy/star_d_link/study/Dto/Request/StudyScheduleCreateRequestDto.java
@@ -3,6 +3,7 @@ package com.udemy.star_d_link.study.Dto.Request;
 import com.udemy.star_d_link.study.Entity.RecurrenceType;
 import com.udemy.star_d_link.study.Entity.Study;
 import com.udemy.star_d_link.study.Entity.StudySchedule;
+import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDateTime;
@@ -15,16 +16,14 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class StudyScheduleCreateRequestDto {
 
-    @NotNull
-    private Long studyId;
-
     @NotEmpty(message = "스터디 제목은 필수 입력 항목입니다.")
     private String scheduleTitle;
 
     @NotEmpty(message = "스터디 내용은 필수 입력 항목입니다.")
     private String scheduleContent;
 
-    @NotEmpty(message = "스터디 날짜는 필수 입력 항목입니다.")
+    @NotNull(message = "스터디 날짜는 필수 입력 항목입니다.")
+    @Future(message = "스터디 날짜는 미래 시간이어야합니다.")
     private LocalDateTime scheduleDate;
 
     private String location;
