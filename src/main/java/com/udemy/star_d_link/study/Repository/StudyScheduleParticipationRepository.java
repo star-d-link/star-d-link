0cd97460a4fae98b5be79069b05c81cb9d8ed96b
GunBan00 <58806686+GunBan00@users.noreply.github.com>
2024-12-20 09:29:26 +0900

    feat: 스터디 스케쥴 작성 시 참여 여부 추가

--- src/main/java/com/udemy/star_d_link/study/Repository/StudyScheduleParticipationRepository.java
+++ src/main/java/com/udemy/star_d_link/study/Repository/StudyScheduleParticipationRepository.java
@@ -1,10 +1,13 @@
 package com.udemy.star_d_link.study.Repository;
 
 import com.udemy.star_d_link.study.Entity.StudyScheduleParticipation;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface StudyScheduleParticipationRepository extends JpaRepository<StudyScheduleParticipation, Long> {
-
+    List<StudyScheduleParticipation> findByStudySchedule_ScheduleId(Long scheduleId);
+    Optional<StudyScheduleParticipation> findByStudySchedule_ScheduleIdAndUsername(Long studyScheduleId, String username);
 }
